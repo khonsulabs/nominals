5b6dabf25039ebd701fde7c28f3ff7217c6ab075
kl-botsu <botsu@khonsulabs.com>
2024-07-25 15:26:46 +0000

    Deploying to gh-pages from @ khonsulabs/nominals@238745c78b0d5f0720a094886b417e8332b07717 🚀

--- main/src-files.js
+++ main/src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["nominals",["",[],["additive.rs","chinese.rs","digital.rs","ethiopic.rs","hebrew.rs","lib.rs","nominalstring.rs"]]]\
+["nominals",["",[],["additive.rs","chinese.rs","digital.rs","enumerated.rs","ethiopic.rs","hebrew.rs","lib.rs","nominalstring.rs"]]]\
 ]'));
 createSrcSidebar();
